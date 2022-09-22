e809f04c8f8bd369ac50922963965a60c4882bd6
shubhamdubile <66626803+shubhamdubile@users.noreply.github.com>
2022-09-23 00:21:52 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,22 +1,50 @@
-from pickle import FRAME
-from queue import Empty
-# from turtle import shape
 import streamlit as st
-import cv2 #computer vision
-import pandas as pd
-#import cvlib as cv
-import numpy as np 
-import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
 import tensorflow as tf
-#from tensorflow.keras.preprocessing.image import img_to_array
-# from tensorflow.keras.model import l
-#from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
-# from statistics import mode
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras import preprocessing
+import time
+import cv2
+
+fig = plt.figure()
+
+with open("custom.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+st.title('Bag Classifier')
+
+st.markdown("Welcome to this simple web application that classifies bags. The bags are classified into six different classes namely: Backpack, Briefcase, Duffle, Handbag and Purse.")
+
+
+def main():
+    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
+    class_btn = st.button("Classify")
+    if file_uploaded is not None:    
+        image = cv2.imread(file_uploaded,1)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+    if class_btn:
+        if file_uploaded is None:
+            st.write("Invalid command, please upload an image")
+        else:
+            with st.spinner('Model working....'):
+                plt.imshow(image)
+                plt.axis("off")
+                predictions = predict(image)
+                time.sleep(1)
+                st.success('Classified')
+                st.write(predictions)
+                st.pyplot(fig)
+
 
 def predict(image):
     classifier_model = "new_vgg19.h5"
     IMAGE_SHAPE = (224, 224,3)
-    model = tf.keras.models.load_model(classifier_model)
+    model = load_model(classifier_model, compile=False, custom_objects={'KerasLayer': hub.KerasLayer})
     test_image = cv2.resize(image, (224,224),
                interpolation = cv2.COLOR_RGB2BGR)
     # test_image = preprocessing.image.img_to_array(test_image)
@@ -33,26 +61,21 @@ def predict(image):
     #scores = tf.nn.softmax(predictions[0])
     #scores = scores.numpy()
     scores=np.argmax(predictions, axis=1)
-    st.text(scores[0])
-    results = class_names[scores[0]]
+    results = class_names[scores]
 
     
     #result = f"{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence." 
     return results
 
-uploaded_file = st.file_uploader("Choose a image file")
-st.text(uploaded_file)
-st.image(uploaded_file)
-if uploaded_file  is not None:
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        
-    opencv_image = cv2.imdecode(file_bytes, 1)
-    Genrate_pred = st.button("Generate Prediction")
-    if Genrate_pred:
-        st.text(predict(opencv_image))
-    
+
+
+
+
+
 
 
 
     
 
+if __name__ == "__main__":
+    main()
